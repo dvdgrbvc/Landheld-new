04e79dfb503b6bd464d6605633aa2cb7d83e8d91
Hasham-dev <vakanihasham64@gmail.com>
2022-11-22 02:12:09 +0500

    updates

--- server.js
+++ server.js
@@ -7,42 +7,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
+if (process.env.NODE_ENV === "production") {
+  //   // dbParams.connectionString = process.env.DATABASE_URL;
+  //   // dbParams.ssl = { rejectUnauthorized: false };
+  //   //server static content
+  //   //npm run build
+  app.use(express.static(path.join(__dirname, "build")));
+}
+
 app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: "dgrabovac3@gmail.com",
-      pass: "mqzikrpbwenggpkl",
-    },
-  });
-  
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+  service: "gmail",
+  auth: {
+    user: "dgrabovac3@gmail.com",
+    pass: "mqzikrpbwenggpkl",
+  },
+});
 
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
 
-  router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message; 
-    const mail = {
-      from: name,
-      to: "david.grabovac@gmx.net",
-      subject: "Contact Form Submission",
-      html: `<p>Name: ${name}</p>
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: "david.grabovac@gmx.net",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
              <p>Email: ${email}</p>
              <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-      if (error) {
-        res.json({ status: "ERROR" });
-      } else {
-        res.json({ status: "Message Sent" });
-      }
-    });
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
   });
+});
